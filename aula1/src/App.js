2a0f201d7b51debbd359fb7ba3e464833c9d6152
Bruna Vieira <bruna.vieira@supplylabs.com.br>
2020-06-15 17:25:19 -0300

    Aula 1

---
+++ aula1/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './paginas/Home/Home';
+
+function App() {
+
+  return (
+   <Home />
+  );
+}
+
+export default App;
---
+++ aula1/src/paginas/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <h1 className="fundo"> Home </h1>
+    );
+}
+export default Home;
