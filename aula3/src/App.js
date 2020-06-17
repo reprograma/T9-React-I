5ed3fc97d1c77a2f951d6d8a782258e1f9def796
Bruna Vieira <bruna.vieira@supplylabs.com.br>
2020-06-17 14:19:33 -0300

    criando projeto do 0

---
+++ aula3/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Input from './componentes/Input/Input';
+
+function App() {
+  return (
+    <>
+      <h1> Oi </h1>
+      <Input />
+    </>
+  );
+}
+
+export default App;
